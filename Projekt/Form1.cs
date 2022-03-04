632dbec032b03335761e84a7dc976d6bfff423dc
unknown <xlaban01@vutbr.cz>
2022-03-04 16:08:11 +0100

    Tvoj tatko

--- Projekt/Form1.cs
+++ Projekt/Form1.cs
@@ -1,9 +1,44 @@
-namespace Projekt;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
-public partial class Form1 : Form
+namespace Projekt
 {
+    public partial class Form1 : Form
+    {
+    private TextBox textBox1;
+
     public Form1()
     {
         InitializeComponent();
+    }
+
+    private void InitializeComponent()
+        {
+        this.textBox1 = new System.Windows.Forms.TextBox();
+        this.SuspendLayout();
+        // 
+        // textBox1
+        // 
+        this.textBox1.AcceptsReturn = true;
+        this.textBox1.AcceptsTab = true;
+        this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+        this.textBox1.Multiline = true;
+        this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+        // 
+        // Form1
+        // 
+        this.ClientSize = new System.Drawing.Size(500, 500);
+        this.Controls.Add(this.textBox1);
+        this.Text = "Tvoj Tatko Records";
+        this.ResumeLayout(false);
+        this.PerformLayout();
+        }
     }
 }
