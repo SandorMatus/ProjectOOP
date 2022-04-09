cd28d2922b140751c7e70be1964ae40acc218356
SandorMatus <matundor@gmail.com>
2022-04-09 22:26:54 +0200

    Initial design

---
+++ Projekt/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Projekt
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int sequence = 0;
+        private Game mainGame = new Game();
+        public MainWindow()
+        {
+            InitializeComponent();
+            mainGame.gameTitle();
+            TextUp.Text = mainGame.Display;
+        }
+        public void changeEvent(){
+            mainGame.Input = TextDown.Text;
+            if(mainGame.RepeatSequence == 0){
+                mainGame.answers(sequence++);
+            }else if (mainGame.RepeatSequence == 1){
+                mainGame.answers(sequence - 1);
+            }else if (mainGame.RepeatSequence == 2){
+                mainGame.answers(100100);
+                sequence = 0;
+            }else if (mainGame.RepeatSequence == 3){
+                mainGame.answers(011011);
+                sequence = 0;
+            }
+            TextUp.Text = mainGame.Display;
+        }
+
+        private void OnKeyDownHandler(object sender, KeyEventArgs e)
+{
+    if (e.Key == Key.Return)
+    {
+       changeEvent();
+       TextDown.Text = "";
+    }
+}
+    }
+}
--- Projekt/projekt.md
+++
@@ -1,6 +0,0 @@
-Projekt OOP
-- staty, skilly ziadne XP
-- moznost boja fight (automaticka vyhra nad nepriatelom) alebo flee (flee 4 ku 10 sanca ze ujdes)
-- aspon 1 boss level
-- inventory pre key itemy 
-- eventy
